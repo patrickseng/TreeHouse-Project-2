fd9b2dcbf3b6e7ee5be49c498886b0a6b515afa4
patrickseng <patseng88@yahoo.com>
2019-01-25 16:28:38 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,56 @@
-const ul = document.querySelector('.student-list');
+
 const studentsItem = document.getElementsByClassName('student-item');
 const studentsPerPage = 10;
 const pageClass = document.querySelector('.page');
-;
+let page = 1
 
 
 const showPage = (list, page) => {
+
   for ( i = 0; i < list.length ; i += 1){
     if ( i >= ((page * (studentsPerPage)) - (studentsPerPage)) && i <= (page * studentsPerPage - 1))
-      { list[i].style.display = "block" }
+    { list[i].style.display = "block" }
     else
-      { list[i].style.display = "none"  }
+    { list[i].style.display = "none"  }
   }
-}
+
+  };
 
 
 const appendPageLinks = (list) => {
+      const ul = document.createElement("ul");
 
       const maxNumPages = Math.ceil(list.length / studentsPerPage);
 
-      var div = document.createElement("div");
+      const div = document.createElement("div");
         div.classList.add('pagination');
         pageClass.appendChild(div);
-
-      const ul = document.createElement("ul");
         div.appendChild(ul);
 
 
       for ( i = 0; i < maxNumPages ; i += 1) {
           const li = document.createElement('li');
-          const a = document.createElement('a') ;
-          a.textContent = i + 1;
-          a.href = "#"
-          li.appendChild(a);
-          ul.appendChild(li);
-        }
 
-}
-showPage(studentsItem, 4);
+          const a = document.createElement('a') ;
+            a.textContent = i + 1 ;
+            a.href = "#";
+            li.appendChild(a);
+            ul.appendChild(li);
+            };
+
+          const pageButtons = document.querySelectorAll('a');
+            pageButtons[0].className = 'active';
+
+          ul.addEventListener('click', (e) => {
+            if (e.target.tagName == 'A') {
+            showPage(studentsItem, e.target.textContent);
+         }
+
+          pageButtons[0].className += "remove";
+
+      });
+
+   }
+
+showPage(studentsItem, page);
 appendPageLinks(studentsItem);
